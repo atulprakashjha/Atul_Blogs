7cca7f4058e8a3cc9302c7044982bd6b86e1e607
Atul Prakash <56566691+atulprakashjha@users.noreply.github.com>
2020-01-31 11:17:26 -0800

    Registration via db

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,11 @@
 from datetime import datetime
-from blog import db
+from blog import db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model):

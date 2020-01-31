0e127d58a5c7f60ccd417a9fca1d05e68cd6e60f
Atul Prakash <56566691+atulprakashjha@users.noreply.github.com>
2020-01-30 17:14:45 -0800

    First page

---
+++ blog.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Atul Prakash',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Pallavi Jha',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    render_template('about.html', posts=posts)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
